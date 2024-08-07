5e09251f6bd1fea86414138d0bd38e1f498c17bc
/Hxckr5 <168587275+Hxckr5@users.noreply.github.com>
2024-08-07 22:44:52 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,13 @@
-/** @type {import('next').NextConfig} */
+/**
+* @type {import('next').NextConfig}
+*/
 const nextConfig = {
-  reactStrictMode: true,
+  output: "export",
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'assets.aceternity.com',
-        // port: '',
-        // pathname: '/account123/**',
-      },
-
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        // port: '',
-        // pathname: '/account123/**',
-      },
-    ],
+    loader: "akamai",
+    path: "",
   },
+  assetPrefix: "./",
 };
 
 export default nextConfig;
